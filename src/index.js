bab8d1550c7ec817ffcfe3ee3fedc5f854c8a7c0
maartenverheul <maarten.verheul@student.hu.nl>
2020-09-05 13:39:09 +0200

    Position frame

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, screen } = require('electron');
 const path = require('path');
 
 function createWindow () {
+
+  const targetScreen = screen.getPrimaryDisplay().workAreaSize;
+  const [winWidth, winHeight] = [350, 600];
+
+  console.log(winWidth);
+
   const win = new BrowserWindow({
-    width: 350,
-    height: 600,
+    width: winWidth,
+    height: winHeight,
+    x: targetScreen.width - winWidth,
+    y: targetScreen.height - winHeight,
     frame: false,
     resizable: false,
+    show: false,
+    alwaysOnTop: true,
     webPreferences: {
       nodeIntegration: true
     }
   });
 
   win.loadFile(path.join(__dirname, '/views/index.html'));
+
+  win.on('ready-to-show', () => {
+    win.show();
+  })
 }
 
 app.whenReady().then(createWindow);
